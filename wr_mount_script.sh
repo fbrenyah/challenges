81fb8dd09c328f8a4c62ba7fc1461a7abcd487a7
Frank Brenyah <fbrenyah@gmail.com>
2021-07-08 20:43:19 -0700

    Add files via upload

---
+++ wr_mount_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# This script mounts volumes it reads from a yaml file
+# This script needs to run first, to build a script that will be sent to instances
+touch ./mount_volumes.sh
+printf "#!/bin/bash \n" >> ./mount_volumes.sh
+num_dev=$(cat ./*.ml | shyaml get-value server.volumes |grep 'device' |wc -l)
+i=0
+for d in 0..$num_dev; do
+	name=$(cat ./*ml |shyaml get-value server.volumes.$i |grep 'device' |cut -d: -f2)
+	type=$(cat ./*.ml | shyaml get-value server.volumes.$i |grep 'type' |cut -d: -f2)
+	mount=$(cat ./*.ml | shyaml get-value server.volumes.$i |grep 'mount' |cut -d: -f2)
+	((i++))
+	cat >> ./mount_volumes.sh <<EOF
+	sudo mkfs -t $type $name
+	if \$? > 0
+		sudo yum install xfsprogs
+		sudo mkfs -t $type $name
+	sudo mkdir $mount
+	if \$? == 0
+		sudo mount $name $mount
+EOF
+done
+chmod +rx ./mount_volumes.sh
